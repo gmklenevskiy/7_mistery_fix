83af4b551c0874e23a3e0578ce2ada20e2727110
gmklenevskiy <gmklenevskiy@gmail.com>
2017-07-18 21:08:57 +0300

    исправлено

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,11 @@
 from math import sqrt
 
 
-def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    if discriminant < 0:
-        return None, None
-    elif discriminant == 0:
-        return -b/(2*a), None
+def get_roots(a,b,c):
+    discriminant=b**2-4*a*c
+    if discriminant<0:
+        return None,None
+    elif discriminant==0:
+        return -b/(2*a),None
     else:
-        return (-b-sqrt(discriminant))/(2*a), (-b+sqrt(discriminant))/(2*a)
+        return (-b-sqrt(discriminant))/(2*a),(-b+sqrt(discriminant))/(2*a)
